1df9b3ba05e260539b480f69b4966a9308c2223e
OualiS <>
2023-04-04 19:22:21 +0200

    remove node modules

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
-import axios from 'axios';
+import axios from "axios";
 import { Configuration, OpenAIApi } from "openai";
-import githubhook from 'githubhook';
-import GitHub from 'github-api'
+import githubhook from "githubhook";
+import GitHub from "github-api";
 
-
-const gh = new GitHub({ token: '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' });
+const gh = new GitHub({
+  token:
+    "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",
+});
 
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
 const handlePullRequest = async (repo, ref, data) => {
   // this is where we'll post the git commit status message
-    console.log(`got webhook on :  ${repo} with action : ${data.action}`);
+  console.log(`got webhook on :  ${repo} with action : ${data.action}`);
 
-    console.log(data);
+  console.log(data);
 
-
-    if(data.action === 'opened' || data.action === 'synchronize') {
+  if (data.action === "opened" || data.action === "synchronize") {
     // console.log(data.pull_request.diff_url)
 
-    const response = await axios.get(data.pull_request.diff_url)
-    const diffBody = response.data
-    console.log('diffBody', diffBody);
+    const response = await axios.get(data.pull_request.diff_url);
+    const diffBody = response.data;
+    console.log("diffBody", diffBody);
 
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages : [
-        {role : "system", content : "You are an expert in the field of computer science and you will make a verbose summary of the following diff from github. You will diplay your summary by file."}, {role : "user", content : ""},
-        {role : "user", content : diffBody}
-      ]
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an expert in the field of computer science and you will make a verbose summary of the following diff from github. You will diplay your summary by file.",
+        },
+        { role: "user", content: "" },
+        { role: "user", content: diffBody },
+      ],
     });
     // console.log(completion.data.choices[0].message.content);
 
-    const repo = gh.getRepo('OualiS', 'wtdclone-test-repo');
+    const repo = gh.getRepo("OualiS", "wtdclone-test-repo");
     const pull = repo.getPullRequest(data.pull_request.id);
     console.log(pull);
     // pull.listComments(function(err, comments) {
@@ -44,22 +50,17 @@ const handlePullRequest = async (repo, ref, data) => {
     //     if (err) throw err;
     //   });
     // });
-    }
+  }
+};
 
-
-
-
-
-}
-
-function listen(port = 8000, secret = 'yhqniwthma57') {
+function listen(port = 8000, secret = "yhqniwthma57") {
   const hook = githubhook({
     port,
     secret,
-    path: '/',
+    path: "/",
   });
 
-  hook.on('pull_request', handlePullRequest);
+  hook.on("pull_request", handlePullRequest);
   hook.listen();
 }
 
